c2fbf2232159ae7294b2a41ce41c5b70535dda31
prakash gurung <prakashgrg18@gmail.com>
2025-01-28 13:47:41 +0000

    removed list layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
-import ListContainer from "./components/list/list-container";
-import ListLayout from "./components/list/list-layout";
-import TradingPairSearchProvider from "./context/trading-pair-filter-provider";
+import ListContainer from './components/list/list-server';
+import Search from './components/list/search';
+import Toggle from './components/list/toggle';
+import TradingPairSearchProvider from './context/trading-pair-filter-provider';
 
 export default function Home() {
   return (
     <TradingPairSearchProvider>
-      <ListLayout>
+      <div className="m-4">
+        <div className="flex justify-center gap-4 mb-4">
+          <Search />
+          <Toggle />
+        </div>
         <ListContainer />
-      </ListLayout>
+      </div>
     </TradingPairSearchProvider>
   );
 }
